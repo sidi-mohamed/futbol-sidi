8d07152bfbf81caf8c4c1eec7f7a59a2b9585602
Sidi Mohamed Abdalahe <sidi_5291@hotmail.com>
2015-05-14 09:08:32 +0200

    he modificado un fichero

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -11,20 +11,16 @@ def nameindex(name='Stranger'):
 @route('/')
 def buscar():
 	return template('index')
-@route('/bbva')
+@get('/bbva')
 def bbva():
-	return template('clasificacion')
-	doc={'key':'d39d0f99f77d0db10f87e93a7dc1f958','league':'1','req':'tables','format':'json'}
+	return template('bbva')
+	
+@route('/bbva/clasificacion')
+def clasificacion1():
+		doc={'key':'d39d0f99f77d0db10f87e93a7dc1f958','league':'1','req':'tables','format':'json'}
 		r = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params=doc)
 		datos = json.loads(r.text)
 		return template('clasificacion',datos=datos)
-
-#@route('/bbva/clasificacion')
-#def clasificacion1():
-#		doc={'key':'d39d0f99f77d0db10f87e93a7dc1f958','league':'1','req':'tables','format':'json'}
-#		r = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params=doc)
-#		datos = json.loads(r.text)
-#		return template('clasificacion',datos=datos)
 
 import os
 from bottle import TEMPLATE_PATH
