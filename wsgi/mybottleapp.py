51de295088e64171600c629ce13bf7266e5c08e8
Sidi Mohamed Abdalahe <sidi_5291@hotmail.com>
2015-05-14 08:54:57 +0200

    he modificado un fichero

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -21,12 +21,7 @@ def clasificacion1():
 		r = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params=doc)
 		datos = json.loads(r.text)
 		return template('clasificacion',datos=datos)
-@route('/bbva/clasificacion')
-def clasificacion2():
-		doc={'key':'d39d0f99f77d0db10f87e93a7dc1f958','league':'2','req':'tables','format':'json'}
-		r = requests.get("http://www.resultados-futbol.com/scripts/api/api.php", params=doc)
-		datos = json.loads(r.text)
-		return template('clasificacion',datos=datos)
+
 import os
 from bottle import TEMPLATE_PATH
 TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
